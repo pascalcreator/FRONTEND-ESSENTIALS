95f5ebfe13493e9d698fd77fb17768d94879d961
PascalKonijnenburggit config --global user.email <pascal.konijnenburg@rocva.nl>
2020-11-06 15:24:51 +0100

    taak 7

--- 05 - Javascript/taak07 - Intelligente DOM/website/script.js
+++ 05 - Javascript/taak07 - Intelligente DOM/website/script.js
@@ -6,4 +6,7 @@ console.log(window.document);
 // document.querySelector("#posterimg1").src = null;
 
 
-// document.querySelector("#poster1");
+// document.querySelector("#poster1"); 
+var elem = document.querySelector ('#poster1')
+Elem.style.color='purple';
+Elem.style.backgroundColor= '#e5e5e5';
